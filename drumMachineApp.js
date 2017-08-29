d8f56e5f7d9da24e9698224d147ed4507ecf8bab
Resek <matija.resek@gmail.com>
2017-08-29 14:50:19 +0200

    keyEvents set & code cleaned (selection inside 2 sounds functions)

--- drumMachineApp.js
+++ drumMachineApp.js
@@ -2,47 +2,47 @@ var squares = document.getElementsByClassName("squares");
 var soundsButtons = document.getElementsByClassName("soundsMode");
 
 var drumsData = {
-    e: {
+    u: {
         sound: new Howl({
 				src: ['drumSounds/prac-kick.wav']
 			})
     },
-    r: {
+    i: {
         sound: new Howl({
 				src: ['drumSounds/prac-perc-1.wav']
 			})
     },
-    t: {
+    o: {
         sound: new Howl({
 				src: ['drumSounds/prac-hat-3.wav']
 			})
     },
-    d: {
+    h: {
         sound: new Howl({
-				src: ['drumSounds/prac-perc-2.wav']
+				src: ['drumSounds/crispydinner.wav']
 			})
     },
-    f: {
+    j: {
         sound: new Howl({
 				src: ['drumSounds/prac-ride-bell-loud.wav']
 			})
     },
-    g: {
+    k: {
         sound: new Howl({
 				src: ['drumSounds/prac-sidestick-2.wav']
 			})
     },
-    c: {
+    b: {
         sound: new Howl({
 				src: ['drumSounds/prac-snare-2.wav']
 			})
     },
-    v: {
+    n: {
         sound: new Howl({
 				src: ['drumSounds/prac-snare-rimshot.wav']
 			})
     },
-    b: {
+    m: {
         sound: new Howl({
 				src: ['drumSounds/prac-tom.wav']
 			})
@@ -50,54 +50,61 @@ var drumsData = {
 }
 
 var weirdoData = {
-    e: {
+    u: {
         sound: new Howl({
 				src: ['manSounds/cjipie.wav']
 			})
     },
-    r: {
+    i: {
         sound: new Howl({
 				src: ['manSounds/eh.wav']
 			})
     },
-    t: {
+    o: {
         sound: new Howl({
 				src: ['manSounds/houb.wav']
 			})
     },
-    d: {
+    h: {
         sound: new Howl({
 				src: ['manSounds/hruuhb.wav']
 			})
     },
-    f: {
+    j: {
         sound: new Howl({
 				src: ['manSounds/jah.wav']
 			})
     },
-    g: {
+    k: {
         sound: new Howl({
 				src: ['manSounds/oa-h.wav']
 			})
     },
-    c: {
+    b: {
         sound: new Howl({
 				src: ['manSounds/uhraa.wav']
 			})
     },
-    v: {
+    n: {
         sound: new Howl({
 				src: ['manSounds/uoh.wav']
 			})
     },
-    b: {
+    m: {
         sound: new Howl({
 				src: ['manSounds/uueh.wav']
 			})
     },
 }
 
-checkSelection();
+//keyEvents
+window.addEventListener("keydown", keysSounds, false);
+window.addEventListener("keyup", backgroundBlue, false);
+
+//nodelist elements click events
+for (var i = 0; i < squares.length; i++) {   
+    squares[i].addEventListener("click", clickSounds, false);
+}
 
 //change background color & back on mouse click
 for (var i = 0; i < squares.length; i++) {
@@ -117,94 +124,77 @@ for (var i = 0; i < soundsButtons.length; i++) {
         soundsButtons[0].classList.remove ("selected");
         soundsButtons[1].classList.remove ("selected");
         this.classList.add ("selected");        
-        checkSelection();
     });
 }
 
-//play drums sounds for specific element / node from nodeList
-function drumsSounds(e) {
-    var properties = Object.values(drumsData);
-    var target = e.target;   
-    var index = Array.prototype.indexOf.call(squares, target);
-    properties[index].sound.play();
-}
-
-//play weirdo sounds for specific element / node from nodeList
-function weirdoSounds(e) {
-    var properties = Object.values(weirdoData);
-    var target = e.target;
-    var index = Array.prototype.indexOf.call(squares, target);
-    properties[index].sound.play();
-}
-
-//check for selected button & add event listeners for nodes
-function checkSelection () {        
-    for (var i = 0; i < squares.length; i++) {
-        if (soundsButtons[0].classList.contains("selected")) {                               
-            //play sound on mouse click for every div
-            squares[i].removeEventListener("click", weirdoSounds, false);
-            squares[i].addEventListener("click", drumsSounds, false);
-        } else if (soundsButtons[1].classList.contains("selected")) {
-            squares[i].removeEventListener("click", drumsSounds, false);
-            //play sound on mouse click for every div
-            squares[i].addEventListener("click", weirdoSounds, false);            
+//select between drums / weirdo sounds and play correct sound for targeted element
+function clickSounds(e) {
+    var target, prop, index;
+    target = e.target;
+    for (var i = 0; i < soundsButtons.length; i++) {
+        if (soundsButtons[0].classList.contains("selected")) {
+            prop = Object.values(drumsData);
+        } else {
+        prop = Object.values(weirdoData);
         }
     }
+    index = Array.prototype.indexOf.call(squares, target);
+    prop[index].sound.play();
 }
 
-//play sound and change background color on keydown
-document.addEventListener("keydown", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 69) {
-        data.e.sound.play();
-        e.style.background = "red";
-    } if (event.keyCode === 82) {
-        data.r.sound.play();
-        r.style.background = "red";
-    } if (event.keyCode === 84) {
-        data.t.sound.play();
-        t.style.background = "red";
-    } if (event.keyCode === 68) {
-        data.d.sound.play();
-        d.style.background = "red";
-    } if (event.keyCode === 70) {
-        data.f.sound.play();
-        f.style.background = "red";
-    } if (event.keyCode === 71) {
-        data.g.sound.play();
-        g.style.background = "red";
-    } if (event.keyCode === 67) {
-        data.c.sound.play();
-        c.style.background = "red";
-    } if (event.keyCode === 86) {
-        data.v.sound.play();
-        v.style.background = "red";
-    } if (event.keyCode === 66) {
-        data.b.sound.play();
-        b.style.background = "red";
+//select between drums / weirdo sounds for keyEvents & switch between pressed keys
+function keysSounds (e) {
+    var prop;
+    e.preventDefault();    
+    for (var i = 0; i < soundsButtons.length; i++) {
+        if (soundsButtons[0].classList.contains("selected")) {
+            prop = Object.values(drumsData);
+        } else {
+        prop = Object.values(weirdoData);
+        }
+    }    
+    switch(e.code) {
+        case "KeyU":
+        prop[0].sound.play();
+        squares[0].style.background = "red";
+        break;
+        case "KeyI":
+        prop[1].sound.play();
+        squares[1].style.background = "red";
+        break;
+        case "KeyO":
+        prop[2].sound.play();
+        squares[2].style.background = "red";
+        break;
+        case "KeyH":
+        prop[3].sound.play();
+        squares[3].style.background = "red";
+        break;
+        case "KeyJ":
+        prop[4].sound.play();
+        squares[4].style.background = "red";
+        break;
+        case "KeyK":
+        prop[5].sound.play();
+        squares[5].style.background = "red";
+        break;
+        case "KeyB":
+        prop[6].sound.play();
+        squares[6].style.background = "red";
+        break;
+        case "KeyN":
+        prop[7].sound.play();
+        squares[7].style.background = "red";
+        break;
+        case "KeyM":
+        prop[8].sound.play();
+        squares[8].style.background = "red";
+        break;       
     }
-}, false);
+}
 
-//change background color back on keyup
-document.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 69) {    
-        e.style.background = "blue";
-    } if (event.keyCode === 82) {
-        r.style.background = "blue";
-    } if (event.keyCode === 84) {
-        t.style.background = "blue";
-    } if (event.keyCode === 68) {
-        d.style.background = "blue";
-    } if (event.keyCode === 70) {
-        f.style.background = "blue";
-    } if (event.keyCode === 71) {
-        g.style.background = "blue";
-    } if (event.keyCode === 67) {
-        c.style.background = "blue";
-    } if (event.keyCode === 86) {
-        v.style.background = "blue";
-    } if (event.keyCode === 66) {
-        b.style.background = "blue"; 
+function backgroundBlue() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.background = "blue";
     }
-}, false);
+}
